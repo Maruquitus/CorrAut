f393b73978bd6b3b323d927276382fce47f6dc65
Maruquitus <killerredstone1@gmail.com>
2023-02-24 21:03:57 -0300

    Modificação STATIC_URL

--- Web/settings.py
+++ Web/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 if not DEBUG:    
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
